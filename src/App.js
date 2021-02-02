75c8ddd440b5b12d52257050fcb72528cb972f05
Aqib_Iqbal <aqibiqbal48@gmail.com>
2021-02-02 18:12:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -109,7 +109,7 @@ const App = () => {
                         return elem;
                       })
                     )
-                    showAlert("Data Updated SuccessfullyY!", "success");
+                    showAlert("Data Updated Successfully!", "success");
                   });
               }
 
